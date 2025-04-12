ad1df7eddc6abf3faa816c350b039037f74744a9
Landry <sebeogojoel@gmail.com>
2025-04-12 13:29:06 +0100

    Initial commit

---
+++ test/deploy fast api on aws lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DeployFastApiOnAwsLambda from '../lib/deploy fast api on aws lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/deploy fast api on aws lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DeployFastApiOnAwsLambda.DeployFastApiOnAwsLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
